53058b790e62aa0ce01072c80019d7770dada071
Jovanny <jcortezen09@hotmail.com>
2023-11-14 06:33:13 -0600

    Spring boot microservices example
    Example of microservice
    Use spring web
    use spring actuator
    use eureka
    use lombok

---
+++ microservice-course/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO courses (name,teacher) VALUES ('Matematicas','Chloe Pace');
+INSERT INTO courses (name,teacher) VALUES ('Historia','Alexa Fleming');
+INSERT INTO courses (name,teacher) VALUES ('Ciencias','Finn Clayton');
+INSERT INTO courses (name,teacher) VALUES ('Literatura','Lars Ford');
+INSERT INTO courses (name,teacher) VALUES ('Educacion Fisica','Sophia Barton');
---
+++ microservice-student/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Carolyn','Huber','varius.nam@icloud.com',4);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Justin','Rios','magna@outlook.com',2);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Jenette','Baird','vitae@yahoo.edu',8);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Noelle','Alexander','est@yahoo.org',5);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Ivory','Mosley','enim.etiam@yahoo.net',5);
